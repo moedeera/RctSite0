0130e5a4a306333a2b70be26165ca2382f404bcc
moedeera <82620264+moedeera@users.noreply.github.com>
2021-12-10 12:04:05 -0600

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import Homepage from './components/Homepage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       MERN app
     </div>
+    </Router>
   );
 }
 
