8338f8ec38487391d935bb3ea352a6a4b2853d4a
moedeera <82620264+moedeera@users.noreply.github.com>
2021-12-19 20:30:46 -0600

    commit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,15 +2,6 @@ import React from 'react'
 import { useState } from 'react';
 export const Navbar = () => {
 
-const [state,setState]=useState(0);
-console.log(state)
-const Change = (x) =>{
-
-
-    setState(x)
-
-    console.log(x)
-}
 
 
 
@@ -20,7 +11,7 @@ const Change = (x) =>{
             <h1>DeeraCode</h1>
             <ul className="List">
 
-<li onClick={()=>Change(1)}> <a href="/" >Home</a>   </li>
+<li > <a href="/" >Home</a>   </li>
 <li><a href="/Portfolio">Portfolio</a></li>
 <li><a href="/Contact">Contact</a></li>
 
