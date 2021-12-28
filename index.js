8d2f29abc361e530d549f98a88ac0cfcc64fdb41
Moe Deera <deeramoe@gmail.com>
2021-12-28 00:43:38 -0600

    Revert "Merge branch 'main' of https://github.com/moedeera/RctSite0"
    This reverts commit f10b78563eec22cc1b8e39eb289ad7fd88b58f30, reversing
    changes made to d4f551e9115e1afb348d75382e84596ec17679a3.

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ app.use(express.urlencoded({extended: true}));
 // res.send('its working')
 
 // })
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 
 
 
