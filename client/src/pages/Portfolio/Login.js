e9d45eb2a217ef5bb140e71f733f956f6732910a
moedeera <82620264+moedeera@users.noreply.github.com>
2022-01-25 03:35:18 -0600

    commit

--- client/src/pages/Portfolio/Login.js
+++ client/src/pages/Portfolio/Login.js
@@ -25,11 +25,7 @@ export const Login = ({ setAuth }) => {
         },
       };
       const body = JSON.stringify(formData);
-      const res = await axios.post(
-        "http://localhost:9700/api/users",
-        body,
-        config
-      );
+      const res = await axios.post("/api/users", body, config);
       console.log(res.data);
       Data = res.data;
       // if (res.data){}
