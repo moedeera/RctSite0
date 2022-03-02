29f5ad47680befef3ec4d88b4c7fd1a5347ac0e8
moedeera <82620264+moedeera@users.noreply.github.com>
2022-03-01 21:40:18 -0600

    commit

---
+++ client/src/utils/PostComment.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useState, useEffect, useContext } from "react";
+import axios from "axios";
+
+export const PostComment = () => {
+  const [comment, setComment] = useState([]);
+ 
+
+  return {comment, setComment}
+};
