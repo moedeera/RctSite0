37d370cb1f03660037a70e0715620ebcfe21eff7
moedeera <82620264+moedeera@users.noreply.github.com>
2022-03-10 08:40:22 -0600

    commit

--- client/src/pages/Portfolio/FriendsPage.js
+++ client/src/pages/Portfolio/FriendsPage.js
@@ -19,6 +19,7 @@ export const Connections = ({ SetFriend }) => {
   return (
     <div className="main-prof">
       <Searchbar setAuth={SetFriend} />
+
       <div className="ConnectMain">
         <div className="UpperConnect">
           <div className="LeftUC">
